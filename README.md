a2fc81a85c2b170dcaeb71560be0b6cc124cce4c
josemoncada87 <josemoncada87@hotmail.com>
2022-05-12 07:17:31 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# S15C2-LiveCodingSample
+# Vite, Vitest, P5, TypeScript and ESLint Starter
+
+### First thing first
+```
+npm install
+```
+### Init the Vite server
+```
+npx vite
+```
+### Check the test stuff
+#### In order to test, open another terminal window and then use the following
+```
+npm run test
+```
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import './style.css';
+import p5 from 'p5';
+import Ball from './ball';
+
+const myBall = new Ball(34);
+
+const sketch = (p:p5) => {
+  // eslint-disable-next-line no-param-reassign
+  p.setup = () => {
+    p.createCanvas(500, 500);
+  };
+  // eslint-disable-next-line no-param-reassign
+  p.draw = () => {
+    p.background(0);
+    p.fill(255, 255, 0);
+    p.ellipse(200, 200, 50, 50);
+    myBall.show(p);
+  };
+};
+// eslint-disable-next-line new-cap
+export default new p5(sketch);
